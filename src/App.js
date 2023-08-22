5896ac771ca4bba9c756833acf7c57237116628c
ravigrauniyar <ravi.bim76@hsm.edu.np>
2023-08-22 18:58:54 +0545

    feat: Navbar component created

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './assets/styles/App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+          <Route path='/' element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
